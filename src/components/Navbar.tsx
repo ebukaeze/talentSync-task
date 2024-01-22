33eb049fa7858f9e881775153663979835175730
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2024-01-22 08:37:45 +0100

    chore: added navbar section

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,138 @@
+import React from "react";
+import logo from "../assets/images/logo.svg";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full max-w-[1472px] bg-gray-100 border-gray-200 dark:bg-gray-900 dark:border-gray-700 rounded-full">
+      <div className="max-w-screen-2xl flex flex-auto flex-wrap items-center justify-between mx-auto py-4 px-8">
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src={logo} className="h-6" alt="ClearLink" />
+        </a>
+        <button
+          data-collapse-toggle="navbar-multi-level"
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-multi-level"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <div
+          className="hidden w-full md:block md:w-auto"
+          id="navbar-multi-level"
+        >
+          <ul className="flex flex-col flex-1 font-medium p-4 md:p-0 mt-4 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <button
+                id="dropdownNavbarLink"
+                data-dropdown-toggle="dropdownNavbar"
+                className="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+              >
+                Products{" "}
+                <svg
+                  width="19"
+                  height="18"
+                  viewBox="0 0 19 18"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M5.19183 6.75L9.69183 11.25L14.1918 6.75"
+                    stroke="#667085"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </button>
+              {/* Dropdown menu */}
+            </li>
+            <li>
+              <button
+                id="dropdownNavbarLink"
+                data-dropdown-toggle="dropdownNavbar"
+                className="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+              >
+                Solutions{" "}
+                <svg
+                  width="19"
+                  height="18"
+                  viewBox="0 0 19 18"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M5.19183 6.75L9.69183 11.25L14.1918 6.75"
+                    stroke="#667085"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </button>
+              {/* Dropdown menu */}
+            </li>
+            <li>
+              <button
+                id="dropdownNavbarLink"
+                data-dropdown-toggle="dropdownNavbar"
+                className="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+              >
+                Resources{" "}
+                <svg
+                  width="19"
+                  height="18"
+                  viewBox="0 0 19 18"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M5.19183 6.75L9.69183 11.25L14.1918 6.75"
+                    stroke="#667085"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </button>
+              {/* Dropdown menu */}
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Pricing
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex items-center justify-evenly gap-4">
+          <button className="w-[144px] h-[52px] bg-white py-3.5 px-6 gap-4 rounded-full border">
+            Talk to sales
+          </button>
+          <button className="bg-blue-700  w-[164px] h-[52px] rounded-full border-2 text-white shadow-sm">
+            Sign up for free
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
